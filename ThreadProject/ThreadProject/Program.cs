b8709c0c20f9c4cede757e01eda31624d0f20f63
Shellyaz26 <TheLittleCitizen4@gmail.com>
2020-08-24 22:42:18 +0300

    visual studio project

---
+++ ThreadProject/ThreadProject/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Threading;
+
+namespace ThreadProject
+{
+    class Program
+    {
+        public static int counter = 0;
+        public static int counterMamas = 0;
+        public static int counterEmpire = 0;
+        public static object locker = new object();
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(() => Work("Mamas"));
+            thread1.IsBackground = false;
+
+
+            Thread thread2 = new Thread(() => Work("Empire"));
+            thread2.IsBackground = false;
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+            Console.WriteLine($"We got mamas {counterMamas} times and \nEmpire {counterEmpire} and total counter {counter}");
+        }
+
+        static void Work(string word)
+        {
+
+            while (counter < 10000)
+            {
+
+                if (word == "Mamas" && counter % 2 == 0)
+                {
+                    counterMamas += 1;
+                    Console.WriteLine(word);
+                }
+                else if (word == "Empire" && counter % 2 == 1)
+                {
+                    counterEmpire += 1;
+                    Console.WriteLine(word);
+                }
+                counter++;
+
+
+
+            }
+        }
+
+    }
+}
